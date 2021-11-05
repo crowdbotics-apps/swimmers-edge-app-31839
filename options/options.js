d440da7476f927867d56b56c8e2af69c40d80449
Crowdbotics <team@crowdbotics.com>
2021-11-05 13:28:43 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "swimmers_edge_app_31839",
+  url: "https://swimmers_edge_app_31839.botics.co",
+  api: "https://swimmers_edge_app_31839.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
